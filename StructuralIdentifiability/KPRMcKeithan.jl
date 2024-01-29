e365f512b58d1393a09de8a7d1be9a8461243aeb
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-01-29 17:27:22 +0100

    Update KPRMcKeithan.jl

--- StructuralIdentifiability/KPRMcKeithan.jl
+++ StructuralIdentifiability/KPRMcKeithan.jl
@@ -3,13 +3,25 @@
 
 using SIAN, Logging
 
+# __________ SIN CONOCER NINGÚN PARÁMETRO ________________________________________________________
 # -------------------  N = 1 -------------------
 ode = @ODEmodel(
     #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
     P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t),
     T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t),
-    C0'(t) = - kon * P * T - koff*C0(t),
-    y1(t) = C0(t)
+    C0'(t) = kon * P * T - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff)*C1(t),
+    y1(t) = C1(t)
+)
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
+    C2'(t) = kp*C1(t) - (koff)*C2(t),
+    y1(t) = C2(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
