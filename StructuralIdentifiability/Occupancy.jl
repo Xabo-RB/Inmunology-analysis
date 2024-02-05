11d5d844ea205dd325e5862cdbe9d6b91d491d4f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-02-05 12:38:29 +0100

    Update Occupancy.jl

--- StructuralIdentifiability/Occupancy.jl
+++ StructuralIdentifiability/Occupancy.jl
@@ -53,7 +53,30 @@ ode = @ODEmodel(
     y1(t) = koff/kon + (T*C0(t))/2
 )
 
+ode = @ODEmodel(
+    C0'(t) = - kon(t) * P * T(t) - koff*C0(t),
+    T'(t) = 0,
+    kon'(t) = 0,
+    y1(t) = koff/kon(t) + (T(t)*C0(t))/2,
+    y2(t) = T(t),
+    y3(t) = kon(t)
+)
+
+ode = @ODEmodel(
+    C0'(t) = - kon * P * T(t) - koff*C0(t),
+    T'(t) = 0,
+    y1(t) = koff/kon + (T(t)*C0(t))/2,
+    y2(t) = T(t)
+)
+
+ode = @ODEmodel(
+    C0'(t) = - kon(t) * P * T - koff*C0(t),
+    kon'(t) = 0,
+    y1(t) = koff/kon(t) + (T*C0(t))/2,
+    y2(t) = kon(t)
+)
 
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
 
