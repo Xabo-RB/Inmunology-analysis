6f265ade67f295deb14e30cb9f1d88d0ffb783ee
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-02-08 12:55:05 +0100

    Update KPRMcKeithan.jl

--- StructuralIdentifiability/KPRMcKeithan.jl
+++ StructuralIdentifiability/KPRMcKeithan.jl
@@ -910,3 +910,149 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ CONOCIENDO kon ________________________________________________________
+# -------------------  N = 1 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff)*C1(t),
+    kon'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kon(t)
+)
+
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
+    C2'(t) = kp*C1(t) - (koff)*C2(t),
+    kon'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kon(t)
+)
+
+# -------------------  N = 3 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
+    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
+    C3'(t) = kp*C2(t) - (koff)*C3(t),
+    kon'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kon(t)
+)
+
+# -------------------  N = 4 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t),
+    T'(t) = - kon(t)* P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
+    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
+    C3'(t) = kp*C2(t) - (koff + kp)*C3(t),
+    C4'(t) = kp*C3(t) - (koff)*C4(t),
+    kon'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kon(t)
+)
+
+# -------------------  N = 5 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
+    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
+    C3'(t) = kp*C2(t) - (koff + kp)*C3(t),
+    C4'(t) = kp*C3(t) - (koff + kp)*C4(t),
+    C5'(t) = kp*C4(t) - (koff)*C5(t),
+    kon'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kon(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ CONOCIENDO kp ________________________________________________________
+# -------------------  N = 1 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff)*C1(t),
+    kp'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kp(t)
+)
+
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff + kp(t))*C1(t),
+    C2'(t) = kp(t)*C1(t) - (koff)*C2(t),
+    kp'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kp(t)
+)
+
+# -------------------  N = 3 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff + kp(t))*C1(t),
+    C2'(t) = kp(t)*C1(t) - (koff + kp(t))*C2(t),
+    C3'(t) = kp(t)*C2(t) - (koff)*C3(t),
+    kp'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kp(t)
+)
+
+# -------------------  N = 4 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff + kp(t))*C1(t),
+    C2'(t) = kp(t)*C1(t) - (koff + kp(t))*C2(t),
+    C3'(t) = kp(t)*C2(t) - (koff + kp(t))*C3(t),
+    C4'(t) = kp(t)*C3(t) - (koff)*C4(t),
+    kp'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kp(t)
+)
+
+# -------------------  N = 5 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff + kp(t))*C1(t),
+    C2'(t) = kp(t)*C1(t) - (koff + kp(t))*C2(t),
+    C3'(t) = kp(t)*C2(t) - (koff + kp(t))*C3(t),
+    C4'(t) = kp(t)*C3(t) - (koff + kp(t))*C4(t),
+    C5'(t) = kp(t)*C4(t) - (koff)*C5(t),
+    kp'(t) = 0,
+    y1(t) = T(t),
+    y2(t) = kp(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
