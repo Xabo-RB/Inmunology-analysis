18888151e314e85c80c4d80f9f6afe48507a87d8
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-05-23 18:20:21 +0200

    n

--- StructuralIdentifiability/KPRsustained_T.jl
+++ StructuralIdentifiability/KPRsustained_T.jl
@@ -9,18 +9,6 @@
 using SIAN, Logging
 
 #   ANÁLISIS POR SALIDA:
-#       1. y = Cn
-#           a) Sin conocer params.
-#           b) Conociendo kon, koff, T(t)
-#           c) Conociendo T(t)
-#           d) Conociendo kon
-#           e) Conociendo kp
-#       2. y = EC50
-#           a) Sin conocer params.
-#           b) Conociendo kon, koff, T(t)
-#           c) Conociendo T(t)
-#           d) Conociendo kon
-#           e) Conociendo kp
 #       3. y = T(t)
 #           a) Sin conocer params.
 #           b) Conociendo kon, koff, T(t)
@@ -39,7 +27,7 @@ ode = @ODEmodel(
     C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
     C2'(t) = kp*C1(t) - koff*C2(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C2(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C2(t) + Tast(t)
+    y1(t) = T(t)
 )
 
 # -------------------  N = 3 -------------------
@@ -52,7 +40,7 @@ ode = @ODEmodel(
     C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
     C3'(t) = kp*C2(t) - koff*C3(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C3(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C3(t) + Tast(t)
+    y1(t) = T(t)
 )
 
 # -------------------  N = 4 -------------------
@@ -66,7 +54,7 @@ ode = @ODEmodel(
     C3'(t) = kp*C2(t) - (koff + kp)*C3(t),
     C4'(t) = kp*C3(t) - koff*C4(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C4(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C4(t) + Tast(t)
+    y1(t) = T(t)
 )
 
 # -------------------  N = 5 -------------------
@@ -81,7 +69,7 @@ ode = @ODEmodel(
     C4'(t) = kp*C3(t) - (koff + kp)*C4(t),
     C5'(t) = kp*C4(t) - koff*C5(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C5(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C5(t) + Tast(t)
+    y1(t) = T(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
@@ -98,10 +86,9 @@ ode = @ODEmodel(
     Tast'(t) = koff*C2(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t), 
     kon'(t) = 0,
     kp'(t) = 0,
-    y1(t) = C2(t) + Tast(t),
-    y2(t) = T(t),
-    y3(t) = kon(t),
-    y4(t) = kp(t)
+    y1(t) = T(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t)
 )
 
 # -------------------  N = 3 -------------------
@@ -116,10 +103,9 @@ ode = @ODEmodel(
     Tast'(t) = koff*C3(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t),
     kon'(t) = 0,
     kp'(t) = 0,
-    y1(t) = C3(t) + Tast(t),
-    y2(t) = T(t),
-    y3(t) = kon(t),
-    y4(t) = kp(t)
+    y1(t) = T(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t)
 )
 
 # -------------------  N = 4 -------------------
@@ -135,10 +121,9 @@ ode = @ODEmodel(
     Tast'(t) = koff*C4(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t),
     kon'(t) = 0,
     kp'(t) = 0,
-    y1(t) = C4(t) + Tast(t),
-    y2(t) = T(t),
-    y3(t) = kon(t),
-    y4(t) = kp(t)
+    y1(t) = T(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t)
 )
 
 # -------------------  N = 5 -------------------
@@ -155,71 +140,9 @@ ode = @ODEmodel(
     Tast'(t) = koff*C5(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t),
     kon'(t) = 0,
     kp'(t) = 0,
-    y1(t) = C5(t) + Tast(t),    
-    y2(t) = T(t),
-    y3(t) = kon(t),
-    y4(t) = kp(t)
-)
-
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
-
-# __________ T(t) ________________________________________________________
-# -------------------  N = 2 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) - kon * P(t) * Tast(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + lambda * Tast(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t),
-    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
-    C2'(t) = kp*C1(t) - koff*C2(t) + kon*P(t)*Tast(t),
-    Tast'(t) = koff*C2(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C2(t) + Tast(t),    
-    y2(t) = T(t)
-)
-
-# -------------------  N = 3 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) - kon * P(t) * Tast(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + lambda * Tast(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t),
-    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
-    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
-    C3'(t) = kp*C2(t) - koff*C3(t) + kon*P(t)*Tast(t),
-    Tast'(t) = koff*C3(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C3(t) + Tast(t),    
-    y2(t) = T(t)
-)
-
-# -------------------  N = 4 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) - kon * P(t) * Tast(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + lambda * Tast(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t),
-    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
-    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
-    C3'(t) = kp*C2(t) - (koff + kp)*C3(t),
-    C4'(t) = kp*C3(t) - koff*C4(t) + kon*P(t)*Tast(t),
-    Tast'(t) = koff*C4(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C4(t) + Tast(t),    
-    y2(t) = T(t)
-)
-
-# -------------------  N = 5 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t) - kon * P(t) * Tast(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + lambda * Tast(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t),
-    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
-    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
-    C3'(t) = kp*C2(t) - (koff + kp)*C3(t),
-    C4'(t) = kp*C3(t) - (koff + kp)*C4(t),
-    C5'(t) = kp*C4(t) - koff*C5(t) + kon*P(t)*Tast(t),
-    Tast'(t) = koff*C5(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
-    y1(t) = C5(t) + Tast(t),    
-    y2(t) = T(t)
+    y1(t) = T(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
@@ -235,7 +158,7 @@ ode = @ODEmodel(
     C2'(t) = kp*C1(t) - koff*C2(t) + kon(t)*P(t)*Tast(t),
     Tast'(t) = koff*C2(t) - kon*P(t)*Tast(t) - lambda*Tast(t),
     kon'(t) = 0, 
-    y1(t) = C2(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kon(t)
 )
 
@@ -250,7 +173,7 @@ ode = @ODEmodel(
     C3'(t) = kp*C2(t) - koff*C3(t) + kon(t)*P(t)*Tast(t),
     Tast'(t) = koff*C3(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t), 
     kon'(t) = 0, 
-    y1(t) = C3(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kon(t)
 )
 
@@ -266,7 +189,7 @@ ode = @ODEmodel(
     C4'(t) = kp*C3(t) - koff*C4(t) + kon(t)*P(t)*Tast(t),
     Tast'(t) = koff*C4(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t), 
     kon'(t) = 0, 
-    y1(t) = C4(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kon(t)
 )
 
@@ -283,12 +206,11 @@ ode = @ODEmodel(
     C5'(t) = kp*C4(t) - koff*C5(t) + kon(t)*P(t)*Tast(t),
     Tast'(t) = koff*C5(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t), 
     kon'(t) = 0, 
-    y1(t) = C5(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kon(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
-
 
 # __________ kp ________________________________________________________
 # -------------------  N = 2 -------------------
@@ -301,7 +223,7 @@ ode = @ODEmodel(
     C2'(t) = kp(t)*C1(t) - koff*C2(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C2(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
     kp'(t) = 0,
-    y1(t) = C2(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kp(t)
 )
 
@@ -316,7 +238,7 @@ ode = @ODEmodel(
     C3'(t) = kp(t)*C2(t) - koff*C3(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C3(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
     kp'(t) = 0,
-    y1(t) = C3(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kp(t)
 )
 
@@ -332,7 +254,7 @@ ode = @ODEmodel(
     C4'(t) = kp(t)*C3(t) - koff*C4(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C4(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
     kp'(t) = 0,
-    y1(t) = C4(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kp(t)
 )
 
@@ -349,7 +271,7 @@ ode = @ODEmodel(
     C5'(t) = kp(t)*C4(t) - koff*C5(t) + kon*P(t)*Tast(t),
     Tast'(t) = koff*C5(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
     kp'(t) = 0,
-    y1(t) = C5(t) + Tast(t),
+    y1(t) = T(t),
     y2(t) = kp(t)
 )
 
