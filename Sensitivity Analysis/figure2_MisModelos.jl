3d71dcc711bd554a4a4b003180b2e6fd5c4c1b1e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-06-19 13:00:36 +0200

    Update figure2_MisModelos.jl

--- Sensitivity Analysis/figure2_MisModelos.jl
+++ Sensitivity Analysis/figure2_MisModelos.jl
@@ -4,8 +4,7 @@ using Revise, QuadGK, Polyester, Enzyme, PyPlot
 # El paquete BioSimulator hay que instalarlo a mano desde el repositorio de github
 using BioSimulator
 using ForwardDiff, DifferentialEquations, DataFrames, DiffEqSensitivity, Sundials, LaTeXStrings, NumericIO
-using ForwardDiff: Chunk, JacobianConfig
-# El paquete BioSimulator hay que instalarlo a mano desde el repositorio de github
+# El paquete AnalyticSensitivity hay que instalarlo a mano desde el repositorio de github
 using AnalyticSensitivity
 
 ASPkg = AnalyticSensitivity
@@ -32,26 +31,6 @@ function sensitivity(x0, p, d, tspan)
     return solution
 end
 
-function ODE(dx, x, p, t) # CARRGO model
-    dx[1] = p[4] * x[1] * (1 - x[1] / p[5]) - p[1] * x[1] * x[2]
-    dx[2] = p[2]* x[1] * x[2] - p[3] * x[2]
-end
-
-function ODEOccupancy(dx, x, p, t) # Occuppancy model
-    dx[1] = p[1]*p[2]*p[3] - p[4]*x[1]
-end
-
-function ODEKPRmcK(dx, x, p, t) # McKeithan
-    dx[1] = - p[1] * x[1] * x[2] + p[2]*x[3] + p[2]*x[4]
-    dx[2] = - p[1] * x[1] * x[2] + p[2]*x[3] + p[2]*x[4]
-    dx[3] = p[1] * x[1] * x[2] - (p[2] + p[3])*x[3]
-    dx[4] = p[3]*x[3] - p[2]*x[4] #C1 = R
-
-    #   p = complex([5e-5, 0.01, 0.01]);
-    #   kon = 5e-5 // koff = 0.01 // kp = 0.01
-    #   x0 = complex([100, 2e4, 0, 0]);
-    #   P = 100 // T = 20.000 // C0 = 0 // C1 = 0
-end
 
 
 
