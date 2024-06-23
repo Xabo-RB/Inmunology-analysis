9d4ac2757d6035a41cef8104e7674f429ca4e749
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-06-23 13:34:38 +0200

    Update figure2_MisModelos.jl

--- Sensitivity Analysis/figure2_MisModelos.jl
+++ Sensitivity Analysis/figure2_MisModelos.jl
@@ -3,10 +3,11 @@ using DifferentialEquations
 using DataFrames, LaTeXStrings, ForwardDiff, DiffEqSensitivity, Sundials, NumericIO
 #, DelimitedFiles, #, BenchmarkTools, Revise, PyPlot
 # El paquete BioSimulator, AnalyticSensitivity hay que instalarlo a mano desde el repositorio de github
-using BioSimulator, AnalyticSensitivity
+# Pkg.add(url="https://github.com/alanderos91/BioSimulator.jl.git")
+using BioSimulator#, AnalyticSensitivity
 
-ASPkg = AnalyticSensitivity
-versioninfo()
+#ASPkg = AnalyticSensitivity
+#versioninfo()
 
 include("Modelos.jl")
 
@@ -38,21 +39,31 @@ if case == 1
         return solution
     end
 
-    p = complex([5e-5, 0.01]); # kon koff 
+    p = complex([5e-5, 0.01]); # kon koff  (afinidad = 0.005)
     x0 = complex([0, 100, 2e4]); # initial values
-    (d, tspan) = (1.0e-16, (0.0,1000)); # step size and time interval in days
+    (d, tspan) = (1.0e-16, (0.0,5000)); # step size and time interval in days
     solution = sensitivity(x0, p, d, tspan); # find solution and partials
 
-    p1 = complex([5e-5, 1]); # kon koff 
+    (d, tspan) = (1.0e-16, (0.0,100)); # step size and time interval in days
+    p1 = complex([0.0005, 0.1]); # kon koff (afinidad = 0.005)
     solution1 = sensitivity(x0, p1, d, tspan); # find solution and partials
 
-    p2 = complex([5e-5, 0.001]); # kon koff 
-    solution2 = sensitivity(x0, p1, d, tspan); # find solution and partials   
+    (d, tspan) = (1.0e-16, (0.0,10000)); # step size and time interval in days
+    p2 = complex([5e-6, 0.001]); # kon koff (afinidad = 0.005)
+    solution2 = sensitivity(x0, p2, d, tspan); # find solution and partials   
 
-    plot(solution[1][:, 3], label = "x1 (p1)", xlabel= "t", ylabel = "R(t)")
-    plot!(solution1[1][:, 3], label = "x1 (p2)")
-    plot!(solution2[1][:, 3], label = "x1 (p3)")
+    pT = plot(solution[1][:, 3], label = "koff = 0.01", xlabel= "t", ylabel = "R(t)")
+    plot!(solution1[1][:, 3], label = "koff = 0.1")
+    plot!(solution2[1][:, 3], label = "koff = 0.001")
     #Plots.plot(solution[1][:, 3], label = "x1", xlabel= "t", ylabel = "R(t)") #xlims = (tspan[1],tspan[2]))
+    display(pT)
+
+    p1 = plot(solution[1][:, 3], label = "koff = 0.01", xlabel= "t", ylabel = "R(t)", title = "Solución 1")
+    display(p1)
+    p2 = plot(solution1[1][:, 3], label = "koff = 0.1", xlabel= "t", ylabel = "R(t)", title = "Solución 2")
+    display(p2)
+    p3 = plot(solution2[1][:, 3], label = "koff = 0.001", xlabel= "t", ylabel = "R(t)", title = "Solución 3")
+    display(p3)
 
 
 elseif case == 2 # ==============================================================================================================
