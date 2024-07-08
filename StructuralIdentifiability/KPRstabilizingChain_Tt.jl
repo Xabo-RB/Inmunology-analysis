12d247318d39aef9417af62d65604e24183c718c
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-07-08 15:50:32 +0200

    n

--- StructuralIdentifiability/KPRstabilizingChain_Tt.jl
+++ StructuralIdentifiability/KPRstabilizingChain_Tt.jl
@@ -4,6 +4,7 @@
 
 using SIAN, Logging
 
+
 # __________ nada ________________________________________________________
 # -------------------  N = 4 -------------------
 ode = @ODEmodel(
@@ -15,8 +16,73 @@ ode = @ODEmodel(
     C2'(t) = r*kp*C1(t) - ((3/(1 + 2*r))*koff + (r^2)*kp)*C2(t),
     C3'(t) = (r^2)*kp*C2(t) - ((4/(1 + 3*r))*koff + (r^3)*kp)*C3(t),
     C4'(t) = (r^3)*kp*C3(t) - ((5/(1 + 4*r))*koff)*C4(t),
-    y1(t) = C4(t)
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t) + C4(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
 
+# __________ R(t) ________________________________________________________
+# -------------------  N = 3 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t) + (4/(1 + 3*r))*koff*C3(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t) + (4/(1 + 3*r))*koff*C3(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - ((2/(1+r))*koff + rp*kp)*C1(t),
+    C2'(t) = rp*kp*C1(t) - ((3/(1 + 2*r))*koff + (rp^2)*kp)*C2(t),
+    C3'(t) = (3/(1 + 2*r))*kp*C2(t) - (4/(1 + 3*r))*koff*C3(t),
+    y1(t) = C3(t),
+    y2(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ kp ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - ((2/(1+r))*koff + rp*kp(t))*C1(t),
+    C2'(t) = rp*kp(t)*C1(t) - (3/(1 + 2*r))*koff*C2(t),
+    kp'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t),
+    y2(t) = kp(t)
+)
+
+# __________ kon ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - ((2/(1+r))*koff + rp*kp)*C1(t),
+    C2'(t) = rp*kp*C1(t) - (3/(1 + 2*r))*koff*C2(t),
+    kon'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t),
+    y2(t) = kon(t)
+)
+
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+
+# __________  TODOS ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + (2/(1+r))*koff*C1(t) + (3/(1+2*r))*koff*C2(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - ((2/(1+r))*koff + rp*kp(t))*C1(t),
+    C2'(t) = rp*kp(t)*C1(t) - (3/(1 + 2*r))*koff*C2(t),
+    kon'(t) = 0,
+    kp'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
