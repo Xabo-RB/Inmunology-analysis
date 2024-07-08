54089867f7f9d075365a53694f7b0a7b16b59595
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-07-08 12:54:11 +0200

    Update KPRsustained_Tt.jl

--- StructuralIdentifiability/KPRsustained_Tt.jl
+++ StructuralIdentifiability/KPRsustained_Tt.jl
@@ -38,7 +38,8 @@ ode = @ODEmodel(
     kp'(t) = 0,
     y1(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t) + Tast(t),
     y2(t) = kon(t),
-    y3(t) = kp(t)
+    y3(t) = kp(t),
+    y4(t) = C3(t) + Tast(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
@@ -56,6 +57,63 @@ ode = @ODEmodel(
     Tast'(t) = koff*C3(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
     y1(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t) + Tast(t),
     y2(t) = C3(t) + Tast(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ conociendo KON ________________________________________________________
+# -------------------  N = 4 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) - kon(t) * P(t) * Tast(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + lambda * Tast(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp)*C0(t),
+    C1'(t) = kp*C0(t) - (koff + kp)*C1(t),
+    C2'(t) = kp*C1(t) - (koff + kp)*C2(t),
+    C3'(t) = kp*C2(t) - (koff + kp)*C3(t),
+    C4'(t) = kp*C3(t) - koff*C4(t) + kon(t)*P(t)*Tast(t),
+    Tast'(t) = koff*C4(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t), 
+    kon'(t) = 0, 
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t) + C4(t) +  Tast(t),
+    y2(t) = kon(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ conociendo kp ________________________________________________________
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) - kon * P(t) * Tast(t),
+    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + lambda * Tast(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff + kp(t))*C1(t),
+    C2'(t) = kp(t)*C1(t) - (koff + kp(t))*C2(t),
+    C3'(t) = kp(t)*C2(t) - (koff + kp(t))*C3(t),
+    C4'(t) = kp(t)*C3(t) - koff*C4(t) + kon*P(t)*Tast(t),
+    Tast'(t) = koff*C4(t) - kon*P(t)*Tast(t) - lambda*Tast(t), 
+    kp'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t) + C4(t) +  Tast(t),
+    y2(t) = kp(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ conociendo parametros ________________________________________________________
+# -------------------  N = 3 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) - kon(t) * P(t) * Tast(t),
+    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + lambda * Tast(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp(t))*C0(t),
+    C1'(t) = kp(t)*C0(t) - (koff + kp(t))*C1(t),
+    C2'(t) = kp(t)*C1(t) - (koff + kp(t))*C2(t),
+    C3'(t) = kp(t)*C2(t) - koff*C3(t) + kon(t)*P(t)*Tast(t),
+    Tast'(t) = koff*C3(t) - kon(t)*P(t)*Tast(t) - lambda*Tast(t),
+    kon'(t) = 0,
+    kp'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t) + C3(t) + Tast(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t),
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
