b59533c25cac64bb5d68679b5a58ae4b374d2cce
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-07-29 13:27:06 +0200

    Update KPRnegative.jl

--- StructuralIdentifiability/KPRnegative.jl
+++ StructuralIdentifiability/KPRnegative.jl
@@ -1,6 +1,6 @@
-#François, P., Voisinne, G., Siggia, E. D., Altan-Bonnet, G., & Vergassola, M. (2013). 
-#Phenotypic model for early T-cell activation displaying sensitivity, specificity, and antagonism. 
-#Proceedings of the National Academy of Sciences, 110(10), E888-E897.
+# Gauud, G., Achar, S., Bourassa, F. X., Davies, J., Hatzihristidis, T., Choi, S., ... & Love, P. E. (2023). 
+#CD3ζ ITAMs enable ligand discrimination and antagonism by inhibiting TCR signaling in response to low-affinity 
+#peptides. _Nature Immunology_, _24_(12), 2121-2134.
 
 using SIAN, Logging
 
@@ -29,57 +29,15 @@ using SIAN, Logging
 # -------------------  N = 2 -------------------
 ode = @ODEmodel(
     #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t) + (b + gamma*S(t))*C1(t),
-    C1'(t) = kp*C0(t) - (koff + kp + b + gamma*S(t))*C1(t) + (b + gamma*S(t))*C2(t),
-    C2'(t) = kp*C1(t) - (koff + b + gamma*S(t))*C2(t),
-    S'(t) = alpha*C1(t)*(ST - S(t)) - beta*S(t), 
-    y1(t) = C2(t)
+    C0'(t) = k * (L1 - C0(t) - C1(t) - C2(t)) * (R - C0(t) - C1(t) - C2(t) - D0(t)- D1(t) - D2(t)) + - ((1/tao1) + phi) * C0(t) + (b + gamma*S(t)),
+    C1'(t) = phi*C0(t) + (b + gamma*S(t))*C2(t) - ((1/tao1) + phi + b + gamma*S(t))*C1(t),
+    C2'(t) = phi*C1(t) - ((1/tao1) + b + gamma*S(t))*C2(t),
+    S'(t) = alpha*(C1(t)+D1(t))*(ST - S(t)) - beta*S(t),
+    D0'(t) = k * (L2 - D0(t) - D1(t) - D2(t)) * (R - C0(t) - C1(t) - C2(t) - D0(t)- D1(t) - D2(t)) + - ((1/tao2) + phi) * D0(t) + (b + gamma*S(t)),
+    D1'(t) = phi*D0(t) + (b + gamma*S(t))*D2(t) - ((1/tao2) + phi + b + gamma*S(t))*D1(t),
+    D2'(t) = phi*D1(t) - ((1/tao2) + b + gamma*S(t))*D2(t),
+    y1(t) = C2(t) + D2(t)
 )
-
-# -------------------  N = 3 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t) + (b + gamma*S(t))*C1(t),
-    C1'(t) = kp*C0(t) - (koff + kp + b + gamma*S(t))*C1(t) + (b + gamma*S(t))*C2(t),
-    C2'(t) = kp*C1(t) - (koff + kp + b + gamma*S(t))*C2(t) + (b + gamma*S(t))*C3(t),
-    C3'(t) = kp*C2(t) - (koff + b + gamma*S(t))*C3(t),
-    S'(t) = alpha*C1(t)*(ST - S(t)) - beta*S(t), 
-    y1(t) = C3(t)
-)
-
-# -------------------  N = 4 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t) + (b + gamma*S(t))*C1(t),
-    C1'(t) = kp*C0(t) - (koff + kp + b + gamma*S(t))*C1(t) + (b + gamma*S(t))*C2(t),
-    C2'(t) = kp*C1(t) - (koff + kp + b + gamma*S(t))*C2(t) + (b + gamma*S(t))*C3(t),
-    C3'(t) = kp*C2(t) - (koff + kp + b + gamma*S(t))*C3(t) + (b + gamma*S(t))*C4(t),
-    C4'(t) = kp*C3(t) - (koff + b + gamma*S(t))*C4(t),
-    S'(t) = alpha*C1(t)*(ST - S(t)) - beta*S(t), 
-    y1(t) = C4(t)
-)
-
-# -------------------  N = 5 -------------------
-ode = @ODEmodel(
-    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t),
-    T'(t) = - kon * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t) + koff*C3(t) + koff*C4(t) + koff*C5(t),
-    C0'(t) = kon * P(t) * T(t) - (koff + kp)*C0(t) + (b + gamma*S(t))*C1(t),
-    C1'(t) = kp*C0(t) - (koff + kp + b + gamma*S(t))*C1(t) + (b + gamma*S(t))*C2(t),
-    C2'(t) = kp*C1(t) - (koff + kp + b + gamma*S(t))*C2(t) + (b + gamma*S(t))*C3(t),
-    C3'(t) = kp*C2(t) - (koff + kp + b + gamma*S(t))*C3(t) + (b + gamma*S(t))*C4(t),
-    C4'(t) = kp*C3(t) - (koff + kp + b + gamma*S(t))*C4(t) + (b + gamma*S(t))*C5(t),
-    C5'(t) = kp*C4(t) - (koff + b + gamma*S(t))*C5(t),
-    S'(t) = alpha*C1(t)*(ST - S(t)) - beta*S(t), 
-    y1(t) = C5(t)
-)
-
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
 
@@ -88,17 +46,17 @@ ode = @ODEmodel(
 # -------------------  N = 2 -------------------
 ode = @ODEmodel(
     #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
-    P'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
-    T'(t) = - kon(t) * P(t) * T(t) + koff*C0(t) + koff*C1(t) + koff*C2(t),
-    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp(t))*C0(t) + (b + gamma*S(t))*C1(t),
-    C1'(t) = kp(t)*C0(t) - (koff + kp(t) + b + gamma*S(t))*C1(t) + (b + gamma*S(t))*C2(t),
-    C2'(t) = kp(t)*C1(t) - (koff + b + gamma*S(t))*C2(t),
-    S'(t) = alpha*C1(t)*(ST - S(t)) - beta*S(t), 
-    kon'(t) = 0,
-    kp'(t) = 0,
+    C0'(t) = k(t) * (L1 - C0(t) - C1(t) - C2(t)) * (R - C0(t) - C1(t) - C2(t) - D0(t)- D1(t) - D2(t)) + - ((1/tao1) + phi) * C0(t) + (b + gamma*S(t)),
+    C1'(t) = phi*C0(t) + (b + gamma*S(t))*C2(t) - ((1/tao1) + phi + b + gamma*S(t))*C1(t),
+    C2'(t) = phi*C1(t) - ((1/tao1) + b + gamma*S(t))*C2(t),
+    S'(t) = alpha*(C1(t)+D1(t))*(ST - S(t)) - beta*S(t),
+    D0'(t) = k * (L2 - D0(t) - D1(t) - D2(t)) * (R - C0(t) - C1(t) - C2(t) - D0(t)- D1(t) - D2(t)) + - ((1/tao2) + phi) * D0(t) + (b + gamma*S(t)),
+    D1'(t) = phi*D0(t) + (b + gamma*S(t))*D2(t) - ((1/tao2) + phi + b + gamma*S(t))*D1(t),
+    D2'(t) = phi*D1(t) - ((1/tao2) + b + gamma*S(t))*D2(t),
     y1(t) = C2(t),
+    y2(t) = D2(t),
     y2(t) = T(t),
-    y3(t) = kon(t),
+    y3(t) = k(t),
     y4(t) = kp(t)
 )
 
@@ -115,9 +73,7 @@ ode = @ODEmodel(
     kon'(t) = 0,
     kp'(t) = 0,
     y1(t) = C3(t),
-    y2(t) = T(t),
-    y3(t) = kon(t),
-    y4(t) = kp(t)
+
 )
 
 # -------------------  N = 4 -------------------
