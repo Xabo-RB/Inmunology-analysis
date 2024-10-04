3d212b2d0015c3fe16267ecc823721c51c3de35f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-10-04 14:37:33 +0200

    Update KPC.jl

--- StructuralIdentifiability/KPC.jl
+++ StructuralIdentifiability/KPC.jl
@@ -10,7 +10,7 @@ ode = @ODEmodel(
     #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
     T'(t) = -k1 * T(t) * X(t) + k3 * D(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t),
     X'(t) = -k1 * T(t) * X(t) + w*C2(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t) + k3*D(t),
-    Tp'(t) = w*C2(t) - k2*Tp(t)*Q(t) + kmenos2*D(t),
+    Tp'(t) = w*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t),
     D'(t) = k2 * Tp(t) * X(t) - (kmenos2 + k3) * D(t),
     C0'(t) = k1 * T(t) * X(t) - (kmenos1 + w) * C0(t),
     C1'(t) = - kmenos1*C1(t) - w*C1(t) + w*C0(t),
@@ -25,7 +25,7 @@ ode = @ODEmodel(
     #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
     T'(t) = -k1 * T(t) * X(t) + k3 * D(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t),
     X'(t) = -k1 * T(t) * X(t) + w*C2(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t) + k3*D(t),
-    Tp'(t) = w*C2(t) - k2*Tp(t)*Q(t) + kmenos2*D(t),
+    Tp'(t) = w*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t),
     D'(t) = k2 * Tp(t) * X(t) - (kmenos2 + k3) * D(t),
     C0'(t) = k1 * T(t) * X(t) - (kmenos1 + w) * C0(t),
     C1'(t) = - kmenos1*C1(t) - w*C1(t) + w*C0(t),
@@ -36,12 +36,12 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
 
-# -------------------  N = kon -------------------
+# -------------------  kon -------------------
 ode = @ODEmodel(
     #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
     T'(t) = -k1(t) * T(t) * X(t) + k3 * D(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t),
     X'(t) = -k1(t) * T(t) * X(t) + w*C2(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t) + k3*D(t),
-    Tp'(t) = w*C2(t) - k2*Tp(t)*Q(t) + kmenos2*D(t),
+    Tp'(t) = w*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t),
     D'(t) = k2 * Tp(t) * X(t) - (kmenos2 + k3) * D(t),
     C0'(t) = k1(t) * T(t) * X(t) - (kmenos1 + w) * C0(t),
     C1'(t) = - kmenos1*C1(t) - w*C1(t) + w*C0(t),
@@ -53,3 +53,20 @@ ode = @ODEmodel(
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
+# __________ kp ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    T'(t) = -k1 * T(t) * X(t) + k3 * D(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t),
+    X'(t) = -k1 * T(t) * X(t) + w(t)*C2(t) + kmenos1 * C0(t) + kmenos1*C1(t) + kmenos1*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t) + k3*D(t),
+    Tp'(t) = w(t)*C2(t) - k2*Tp(t)*X(t) + kmenos2*D(t),
+    D'(t) = k2 * Tp(t) * X(t) - (kmenos2 + k3) * D(t),
+    C0'(t) = k1 * T(t) * X(t) - (kmenos1 + w(t)) * C0(t),
+    C1'(t) = - kmenos1*C1(t) - w(t)*C1(t) + w(t)*C0(t),
+    C2'(t) = - kmenos1*C2(t) - w(t)*C2(t) + w(t)*C1(t),
+    y1(t) = Tp(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3))
+
