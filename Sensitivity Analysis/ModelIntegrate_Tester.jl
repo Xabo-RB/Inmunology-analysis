0d1e10eb943b8eb55814d7e1b5035645cd93ad24
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-10-07 12:10:46 +0200

    Update ModelIntegrate_Tester.jl

--- Sensitivity Analysis/ModelIntegrate_Tester.jl
+++ Sensitivity Analysis/ModelIntegrate_Tester.jl
@@ -3,22 +3,22 @@ using Plots
 
 include("Modelos.jl")
 
-# Parámetros de la ODE (ajústalos según sea necesario)
+# Parámetros de la ODE
 p = [10, 1, 0.1, 1, 1, 10]
 
-# Condiciones iniciales (ajústalas según sea necesario)
+# Condiciones iniciales
 x0 = [100, 2, 0, 0, 0, 0, 0]
 
-(d, tspan) = (1.0e-11, (0.0,100)); # step size and time interval in days
+(d, tspan) = (1.0e-11, (0.0,700)); # step size and time interval in days
 
 # Resolver la ODE
 prob = ODEProblem(ODEKPC, x0, tspan, p)
 sol = solve(prob, dt=d)
 
-# Extraer y graficar el tercer estado (x[3])
+
 t_vals = sol.t
-x3_vals = sol[3, :]  # x[3] representa a Tp(t) / Rp(t)
+x3_vals = sol[3, :]
 
 # Graficar
-plot(t_vals, x3_vals, label="x[3] (Tp(t) / Rp(t))", xlabel="Tiempo (t)", ylabel="x[3]", lw=2)
-title!("Solución del tercer estado de la ODE")
+plot(t_vals, x3_vals, label="Response", xlabel="Tiempo (t)", ylabel="x[3]", lw=2)
+title!("System Response")
