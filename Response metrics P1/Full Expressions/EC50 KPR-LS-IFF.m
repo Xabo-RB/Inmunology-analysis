35252afd84912c1d40231d33d0f5a47db5974c94
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-04-24 10:03:03 +0200

    n

---
+++ Response metrics P1/Full Expressions/EC50 KPR-LS-IFF.m
@@ -0,0 +1,32 @@
+LTT = LT -> -1/2*(a^3*TT*(KD + TT)*\[Lambda]*\[Mu]*\[CapitalOmega]*
+         (-(PT*(gpp + YT*\[Delta])*\[Lambda]) + (gpm + gpp + YT*\[Delta])*
+           \[Lambda]*\[CapitalOmega] + (gym + gyp)*\[Mu]*\[CapitalOmega]) + 
+        a*(gpp*(gym + gyp)*PT + gyp*PT*YT*\[Delta] - 
+          ((gpm + gpp)*(gym + gyp) + gyp*YT*\[Delta])*\[CapitalOmega])*
+         (PT*(gpp + YT*\[Delta])*\[Lambda] - 
+          ((gpm + gpp + YT*\[Delta])*\[Lambda] + (gym + gyp)*\[Mu])*
+           \[CapitalOmega]) + 
+        Sqrt[(a*PT*(YT*\[Delta]*(gyp + a*TT*\[Lambda]) + gpp*(gym + gyp + 
+                a*TT*\[Lambda])) - a*(YT*\[Delta]*(gyp + a*TT*\[Lambda]) + 
+              gpm*(gym + gyp + a*TT*\[Lambda]) + gpp*(gym + gyp + 
+                a*TT*\[Lambda]) + a*TT*(gym + gyp + a*(KD + TT)*\[Lambda])*
+               \[Mu])*\[CapitalOmega])^2*(PT^2*(gpp + YT*\[Delta])^2*
+            \[Lambda]^2 - 2*PT*\[Lambda]*((gpp + YT*\[Delta])*
+              (gpm + gpp + YT*\[Delta])*\[Lambda] - (gpp*(gym + gyp) + 
+               (-gym + gyp)*YT*\[Delta])*\[Mu])*\[CapitalOmega] + 
+           ((gpm + gpp + YT*\[Delta])^2*\[Lambda]^2 - 
+             2*((gpm + gpp)*(gym + gyp) + (-gym + gyp)*YT*\[Delta])*\[Lambda]*
+              \[Mu] + (gym + gyp)^2*\[Mu]^2)*\[CapitalOmega]^2)] + 
+        a^2*(PT^2*TT*(gpp + YT*\[Delta])^2*\[Lambda]^2 - 
+          2*PT*\[Lambda]*(TT*(gpp + YT*\[Delta])*(gpm + gpp + YT*\[Delta])*
+             \[Lambda] + gpp*(gym + gyp)*(KD + TT)*\[Mu] + 
+            (gym*TT + gyp*(KD + TT))*YT*\[Delta]*\[Mu])*\[CapitalOmega] + 
+          (TT*(gpm + gpp + YT*\[Delta])^2*\[Lambda]^2 + 
+            2*((gpm + gpp)*(gym + gyp)*(KD + TT) + (gym*TT + gyp*(KD + TT))*
+               YT*\[Delta])*\[Lambda]*\[Mu] + (gym + gyp)^2*TT*\[Mu]^2)*
+           \[CapitalOmega]^2))/(a^2*\[Lambda]*\[Mu]*\[CapitalOmega]*
+        (-(PT*YT*\[Delta]*(gyp + a*TT*\[Lambda])) - 
+         gpp*(gym + gyp + a*TT*\[Lambda])*(PT - \[CapitalOmega]) + 
+         gpm*gym*\[CapitalOmega] + (gpm + YT*\[Delta])*(gyp + a*TT*\[Lambda])*
+          \[CapitalOmega] + a*TT*(gym + gyp + a*TT*\[Lambda])*\[Mu]*
+          \[CapitalOmega]))
