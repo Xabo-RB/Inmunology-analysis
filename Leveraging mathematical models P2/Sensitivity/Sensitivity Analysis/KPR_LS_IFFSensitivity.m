3308aa06472bfbe0876646cc5efe9977d8935230
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-07-24 18:07:23 +0200

    a

--- Leveraging mathematical models P2/Sensitivity/Sensitivity Analysis/KPR_LS_IFFSensitivity.m
+++ Leveraging mathematical models P2/Sensitivity/Sensitivity Analysis/KPR_LS_IFFSensitivity.m
@@ -199,7 +199,7 @@ hold on
 function solution = sensitivity(x0, p, d, tspan)
 
     ST = @(t,y)ODELimIFF(t, y, p);
-    options = odeset('RelTol',1e-7,'AbsTol',1e-7, 'Refine', 1);
+    options = odeset('RelTol',1e-9,'AbsTol',1e-9, 'Refine', 1);
     [t,x] = ode15s(ST, tspan, x0, options);
     
     lp = length(p); ls = size(x, 1); lx = length(x0);
@@ -220,7 +220,7 @@ function solution = sensitivity(x0, p, d, tspan)
         % para calcular la derivada parcial de la solución con respecto a ese parámetro.
         p(j) = p(j) + d * 1i; % Perturba el parámetro
         
-        options = odeset('RelTol',1e-7,'AbsTol',1e-7, 'Refine', 1);
+        options = odeset('RelTol',1e-9,'AbsTol',1e-9, 'Refine', 1);
         ST = @(t,y)ODELimIFF(t, y, p);
         [t,x] = ode15s(ST, tspan, x0, options);
         
