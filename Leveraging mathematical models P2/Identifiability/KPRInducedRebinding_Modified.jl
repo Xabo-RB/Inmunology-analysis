27c80a8e9bffdf51a64e754c14d134ba097a9cf7
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-09-16 14:49:20 +0900

    Add new ODE models for identifiability analysis (N=2)
    Introduces several new ODE model definitions for N=2 cases, including scenarios with time-dependent and constant parameters (kon, kp), and different known variables (T(t), kon, R, kp). Each model is followed by an identifiability analysis call. These additions expand the range of identifiability tests that can be performed in the script.

--- Leveraging mathematical models P2/Identifiability/KPRInducedRebinding_Modified.jl
+++ Leveraging mathematical models P2/Identifiability/KPRInducedRebinding_Modified.jl
@@ -38,6 +38,25 @@ ode = @ODEmodel(
 
 @time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
 
+# __________ sin T(t) ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = -kon(t) * P(t) * T(t) + koff * C0(t) + lambdaR * (koff / (koff + rho1)) * C1(t) + lambdaR * (koff / (koff + rho2)) * C2(t),
+    T'(t) = -kon(t) * P(t) * T(t) + koff * C0(t) + lambdaR * (koff / (koff + rho1)) * C1(t) + lambdaR * (koff / (koff + rho2)) * C2(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp(t)) * C0(t),
+    C1'(t) = kp(t) * C0(t) - kp(t) * C1(t) + (koff / (koff + rho1)) * C1(t),
+    C2'(t) = kp(t) * C1(t) - (koff / (koff + rho2)) * C2(t),
+    kon'(t) = 0,
+    kp'(t) = 0,
+    y1(t) = C2(t),
+    y2(t) = kon(t),
+    y3(t) = kp(t)
+)
+
+
+@time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
+
 # __________ CONOCIENDO T(t) ________________________________________________________
 # -------------------  N = 2 -------------------
 ode = @ODEmodel(
@@ -239,6 +258,41 @@ ode = @ODEmodel(
     kp'(t) = 0,
     y1(t) = T(t) + C0(t) + C1(t) + C2(t),
     y2(t) = kp(t)
+)
+
+
+@time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
+
+# __________ CONOCIENDO Kon y R ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = -kon(t) * P(t) * T(t) + koff * C0(t) + lambdaR * (koff / (koff + rho1)) * C1(t) + lambdaR * (koff / (koff + rho2)) * C2(t),
+    T'(t) = -kon(t) * P(t) * T(t) + koff * C0(t) + lambdaR * (koff / (koff + rho1)) * C1(t) + lambdaR * (koff / (koff + rho2)) * C2(t),
+    C0'(t) = kon(t) * P(t) * T(t) - (koff + kp) * C0(t),
+    C1'(t) = kp * C0(t) - kp * C1(t) + (koff / (koff + rho1)) * C1(t),
+    C2'(t) = kp * C1(t) - (koff / (koff + rho2)) * C2(t),
+    kon'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t),
+    y2(t) = kon(t),
+    y3(t) = C2(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
+
+# __________ CONOCIENDO kp ________________________________________________________
+# -------------------  N = 2 -------------------
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    P'(t) = -kon * P(t) * T(t) + koff * C0(t) + lambdaR * (koff / (koff + rho1)) * C1(t) + lambdaR * (koff / (koff + rho2)) * C2(t),
+    T'(t) = -kon * P(t) * T(t) + koff * C0(t) + lambdaR * (koff / (koff + rho1)) * C1(t) + lambdaR * (koff / (koff + rho2)) * C2(t),
+    C0'(t) = kon * P(t) * T(t) - (koff + kp(t)) * C0(t),
+    C1'(t) = kp(t) * C0(t) - kp(t) * C1(t) + (koff / (koff + rho1)) * C1(t),
+    C2'(t) = kp(t) * C1(t) - (koff / (koff + rho2)) * C2(t),
+    kp'(t) = 0,
+    y1(t) = T(t) + C0(t) + C1(t) + C2(t),
+    y2(t) = kp(t),
+    y3(t) = C2(t)
 )
 
 
