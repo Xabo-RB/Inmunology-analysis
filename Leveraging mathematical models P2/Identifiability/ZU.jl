a5bd51ad94f67d6f7d09fbc0e3f17b3a2f4c9e1c
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-09-24 17:09:18 +0900

    Update ZU.jl

--- Leveraging mathematical models P2/Identifiability/ZU.jl
+++ Leveraging mathematical models P2/Identifiability/ZU.jl
@@ -213,7 +213,60 @@ ode = @ODEmodel(
     Q'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + k3 * D(t),
     k1'(t) = 0,
     y1(t) = Tp(t),
-    y2(t) = k1(t)
+    y2(t) = k1(t),
+    y3(t) = T(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
+
+# __________ kp kon  ________________________________________________________
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    T'(t) = -k1(t) * T(t) * L(t) + k3 * D(t) + kmenos1 * C0(t),
+    L'(t) = -k1(t) * T(t) * L(t) + w(t) * C0(t) + kmenos1 * C0(t),
+    C0'(t) = k1(t) * T(t) * L(t) - (kmenos1 + w) * C0(t),
+    D'(t) = k2 * Tp(t) * Q(t) - (kmenos2 + k3) * D(t),
+    Tp'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + w(t) * C0(t),
+    Q'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + k3 * D(t),
+    w'(t) = 0,
+    k1'(t) = 0,
+    y1(t) = Tp(t),
+    y2(t) = w(t),
+    y3(t) = k1(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
+
+# __________ TT R kon ________________________________________________________
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    T'(t) = -k1(t) * T(t) * L(t) + k3 * D(t) + kmenos1 * C0(t),
+    L'(t) = -k1(t) * T(t) * L(t) + w * C0(t) + kmenos1 * C0(t),
+    C0'(t) = k1(t) * T(t) * L(t) - (kmenos1 + w) * C0(t),
+    D'(t) = k2 * Tp(t) * Q(t) - (kmenos2 + k3) * D(t),
+    Tp'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + w * C0(t),
+    Q'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + k3 * D(t),
+    k1'(t) = 0,
+    y1(t) = T(t) + Tp(t) + C0(t) + D(t),
+    y2(t) = k1(t),
+    y3(t) = Tp(t)
+)
+
+@time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
+
+# __________ kp ________________________________________________________
+ode = @ODEmodel(
+    #dPdt (pMHC) / dTdt (TCR) / dC0/dt (1º pMHC-TCR)
+    T'(t) = -k1 * T(t) * L(t) + k3 * D(t) + kmenos1 * C0(t),
+    L'(t) = -k1 * T(t) * L(t) + w(t) * C0(t) + kmenos1 * C0(t),
+    C0'(t) = k1 * T(t) * L(t) - (kmenos1 + w) * C0(t),
+    D'(t) = k2 * Tp(t) * Q(t) - (kmenos2 + k3) * D(t),
+    Tp'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + w(t) * C0(t),
+    Q'(t) = -k2 * Tp(t) * Q(t) + kmenos2 * D(t) + k3 * D(t),
+    w'(t) = 0,
+    y1(t) = Tp(t),
+    y2(t) = w(t),
+    y3(t) = T(t) + Tp(t) + C0(t) + D(t)
 )
 
 @time println(identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=2^29 - 3))
